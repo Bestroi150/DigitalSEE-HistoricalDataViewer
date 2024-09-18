32076f8ee3229545dad9b2aebab161f02d055f63
Kristiyan <bestroi@users.noreply.huggingface.co>
2024-09-18 17:18:29 +0000

    Update apps/map_page.py

--- apps/map_page.py
+++ apps/map_page.py
@@ -9,11 +9,12 @@ def app():
     
     # Author and general information
     st.write(f"""In this demonstration, we utilize a dataset that includes geographic coordinates alongside descriptive information about various locations. Each entry in our dataset comprises the following components:
-
+The map can be expandanded and it's layers can be changed. We utilize a OpenStreetMap and CartoDB(Light and Dark Mode).
 Location: The name of the place, providing a clear reference to the geographic feature or city.
 Latitude: The geographic latitude of the location, allowing us to pinpoint its position on a map.
 Longitude: The geographic longitude, complementing the latitude to ensure precise mapping.
-Description: A brief narrative that encapsulates the essence of the location, offering context and inviting brief information from the data set.""")
+Description: A brief narrative that encapsulates the essence of the location, offering context and inviting brief information from the data set.
+This demonstration shows the potential of the dataset at hand to be used in a GIS-based software suitable for Digital Archaeology research""")
 
     # Load the JSON data
     try:
